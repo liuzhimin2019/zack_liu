a11a1d8773d0d552b60029179dab3208aa876acb
liuzhimin <liu.zhimin.2019@gmail.com>
2021-07-07 16:46:33 +0800

    初始提交

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = '0i@jj^3&uppw-ho^sctz3uz*+@lhb!!t0m8ct^aqrk413i_9o@'
+DEBUG = True
+#EBUG = False
+
+DB_NAME = 'erp'
+DB_USER = 'root'
+DB_PASSWORD = 'Lzm301435!'
+DB_PORT = 3306
+DB_HOST = 'localhost'
+DB_PORT = ''
